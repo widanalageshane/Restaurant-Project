ed49fde89b6b9d07fcb94eee536898d4e0d43f3b
nuwan98765432 <t3sunu00@students.oamk.fi>
2024-04-08 14:11:05 +0300

    Nuwan Post Final commit

---
+++ FrontEnd/JS/addMenu.js
@@ -0,0 +1,80 @@
+import { MenuCardAdd } from './CLASS/addmenucard.js';
+
+const menuCardAdd = new MenuCardAdd('http://localhost:3001');
+
+
+//1.This is for getPost---- catch the card section div in which we want to add the card divs
+const menu_div = document.getElementById("menu_card");
+
+
+
+//---------------------------addPost() event listener --------------------------------------------------------------
+
+//2. This is for Post of Menu to databe--- catch the input fileds form admin post page
+const input_name = document.getElementById("id_name");
+const input_description = document.getElementById("id_description");
+const input_price = document.getElementById("id_price");
+const input_image = document.getElementById("id_image");
+console.log(input_name);
+
+//key press function with above renderTask function.
+const button_done = document.getElementById("button_done");
+
+button_done.onclick = () => {
+//get all input_name, input_description, input_price, input_image values to a variable
+    //alert(input_name.value + input_description.value + input_price.value + input_image.value);
+    //button_done.innerHTML = "Add Menu";
+
+    const name = input_name.value.trim();
+    const description = input_description.value.trim();
+    const price = input_price.value.trim();
+    const image = input_image.value.trim();
+    
+    //alert(name + description + price + image);
+    if(name !== '' && description !== '' && price !== '' && image !== ''){ 
+        // add above name, description, price, image to the menuCard array
+        menuCardAdd.addPost(name, description, price, image)
+        
+        .then((menu) => {
+            alert('Menu added successfully');
+            //console.log(menu);
+            render_menu(menu);
+            //alert('Menu added successfully');
+            //console.log(menu);
+        })
+
+    } else {
+        alert('Please fill all the fields');
+    }    
+        
+};
+
+
+
+
+
+
+
+
+
+//---------------------------DELETE menu Part--------------------------------------------------------------
+
+
+//3. This is for delete to databe--- catch the input fileds form admin post page
+
+const button_delete = document.getElementById("button_delete");
+
+//addeventlistener to delete from menuCard array using removeTask(id) function, once button_delete clicked, with catch error. 
+button_delete.addEventListener('click', () => {
+
+    //get the id value enterd in input field
+    const id = document.getElementById("delete_id").value;
+    if(id !== ''){
+        menuCardAdd.removeTask(id)
+            alert('Menu deleted successfully');
+            //console.log(id);
+    } else {
+        alert('Please fill the id field');
+    }
+});
+
