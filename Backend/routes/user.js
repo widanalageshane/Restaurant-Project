c3f7da300dad2223131eec24d5da095b38ddce08
KPManjula <t3kama07@students.oamk.fi>
2024-04-17 10:15:38 +0300

    Hashing password with bcrypt

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const { query } = require('../helpers/db.js');
+const bcrypt = require('bcrypt');
+
+
 const userRouter = express.Router();
 
 userRouter.post("/login", async (req, res) => {
@@ -7,31 +10,50 @@ userRouter.post("/login", async (req, res) => {
         const sql = "SELECT * FROM account WHERE email = $1";
         const result = await query(sql, [req.body.email]);
         if (result.rowCount === 1) {
-            if (result.rows[0].password === req.body.password) {
-                res.status(200).json(result.rows[0]);
+            bcrypt.compare(req.body.password, result.rows[0].password, (err,bcrypt_res) =>{
+             if(!err){
+                if(bcrypt_res === true){
+                    const user = result.rows[0];
+                    res.status(200).json({"account_id": user.account_id, "email": user.email});
+                } else {
+                    res.statusMessage = 'Invalid login'
+                    res.status(401).json({error: 'Invalid login'})
+                  }
+                } else {
+                  res.statusMessage = err
+                  res.status(500).json({error: err})
+                }
+              })
             } else {
-                res.status(401).json({ error: 'Invalid password' });
+              res.statusMessage = 'Invalid login'
+              res.status(401).json({error: 'Invalid login'})
             }
-        } else {
-            res.status(401).json({ error: 'Invalid email' });
-        }
-    } catch(error) {
-        res.status(500).json({ error: error.message });
-    }
-});
+          } catch (error) {
+            res.statusMessage = error
+            res.status(500).json({error: error})
+          }
+        });
 
 
 
 
 userRouter.post("/register", async (req, res) => {
-    try {
-        const { username, email, password } = req.body;
-        const sql = "INSERT INTO account (username, email, password) VALUES ($1, $2, $3) RETURNING id";
-        const result = await query(sql, [username, email, password]);
-        res.status(200).json({ id: result.rows[0].id });
-    } catch(error) {
-        res.status(500).json({ error: error.message });
-    }
+    bcrypt.hash(req.body.password, 10, async (err, hash) => {
+        if(!err){
+            try {
+                const sql = "INSERT INTO account (username, email, password) VALUES ($1, $2, $3) RETURNING account_id";
+                const result = await query(sql, [req.body.username, req.body.email, hash]);
+                res.status(201).json({ account_id: result.rows[0].account_id });
+            } catch(error) {
+                res.statusMessage = error;
+                res.status(500).json({ error: error });
+            }
+            } else {    
+                res.statusMessage = err;
+                res.status(500).json({ error: err });
+            }
+     });
+   
 });
 
 
